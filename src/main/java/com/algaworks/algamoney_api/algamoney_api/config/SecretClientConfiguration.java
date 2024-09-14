6c58ad9036853aa24edbb3558437a2ba6fafb3b9
flaviohenso <flavio.henso@gmail.com>
2024-09-14 05:09:13 -0300

    Configurando o acesso ao Keyvault de acordo com o profile

--- src/main/java/com/algaworks/algamoney_api/algamoney_api/config/SecretClientConfiguration.java
+++ src/main/java/com/algaworks/algamoney_api/algamoney_api/config/SecretClientConfiguration.java
@@ -1,13 +1,17 @@
 package com.algaworks.algamoney_api.algamoney_api.config;
 
 
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Profile;
+
 import com.azure.identity.DefaultAzureCredentialBuilder;
 import com.azure.security.keyvault.secrets.SecretClient;
 import com.azure.security.keyvault.secrets.SecretClientBuilder;
 
-
+@Profile("prod")
 public class SecretClientConfiguration {
 
+    @Bean
     public SecretClient secretClient() {
         return new SecretClientBuilder()
             .vaultUrl(System.getenv("APP_CONFIGURATION_ENDPOINT"))
